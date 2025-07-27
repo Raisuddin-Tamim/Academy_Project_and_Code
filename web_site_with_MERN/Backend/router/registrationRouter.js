12b52aa42f5dc4eb629963f38f6c865764e2a713
Raisuddin Tamim <tamimmood@gmail.com>
2025-07-27 17:05:15 +0600

    delete duplicate

--- web_site_with_MERN/Backend/router/registrationRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { registerInfo } from "../controller/registerController.js";
-
-const router = express.Router();
-
-router.post("/register", registerInfo);
-
-export default router;
--- web_site_with_MERN/Backend/router/signupRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { signupInfo } from "../controller/SignUpController.js";
-
-const router = express.Router();
-
-router.post("/signup", signupInfo);
-
-export default router;
